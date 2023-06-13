2fa559ef1991f654a4d65064004e91217504d3cb
Luke Wong <lukewong93@gmail.com>
2023-06-13 13:37:07 +0800

    feat: fetch all users

--- app/components/UserCards.tsx
+++ app/components/UserCards.tsx
@@ -2,53 +2,95 @@
 
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Skeleton from "@mui/material/Skeleton";
+import useUsers from "../hooks/useUsers";
+import dayjs from "dayjs"
+import { Fragment, useEffect, useState } from "react";
 
 export default function UserCards() {
+  // const [users, setUsers] = useState([]);
+  const { users, isLoading } = useUsers(7);
+
+  console.log("users:", users);
+
   return (
     <section>
-      <Box
-        sx={{
-          padding: "2rem 1.75rem",
-          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
-          backgroundColor: "#ffffff",
-          borderRadius: "0.5rem",
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
-          cursor: "pointer",
-          justifyItems: "center"
-        }}
-      >
-        <Typography
-          variant="body2"
-          sx={{ color: "#979797", fontWeight: "400" }}
-        >
-          14 Sep 2022
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ color: "#303030", fontWeight: "600" }}
-        >
-          Megan R.Johnson
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ color: "#979797", fontWeight: "400" }}
-        >
-          Female
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ color: "#303030", fontWeight: "400" }}
-        >
-          Australia
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ color: "#979797", fontWeight: "400" }}
-        >
-          meganejohnson@gmail.com
-        </Typography>
-      </Box>
+      {isLoading ? (
+        <Fragment>
+          <Skeleton
+            sx={{ borderRadius: "0.5rem", marginBottom: "1rem" }}
+            variant="rectangular"
+            width={"100%"}
+            height={80}
+          />
+          <Skeleton
+            sx={{ borderRadius: "0.5rem", marginBottom: "1rem" }}
+            variant="rectangular"
+            width={"100%"}
+            height={80}
+          />
+          <Skeleton
+            sx={{ borderRadius: "0.5rem" }}
+            variant="rectangular"
+            width={"100%"}
+            height={80}
+          />
+        </Fragment>
+      ) : (
+        // TODO: Fix TypeScript error
+        // @ts-ignore
+        users.map((user, index) => (
+          <Box
+            key={index}
+            sx={{
+              padding: "2rem 1.75rem",
+              boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+              backgroundColor: "#ffffff",
+              borderRadius: "0.5rem",
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
+              cursor: "pointer",
+              justifyItems: "center",
+              marginBottom: "1rem",
+              border: "2px solid #ffffff",
+              ":hover": {
+                borderColor: "#35bad8",
+              }
+            }}
+          >
+            <Typography
+              variant="body2"
+              sx={{ color: "#979797", fontWeight: "400" }}
+            >
+              {dayjs(user.dob.date).format("DD MMM YYYY")}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "#303030", fontWeight: "600" }}
+            >
+              {user.name.first} {user.name.last}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "#979797", fontWeight: "400" }}
+            >
+              {user.gender}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "#303030", fontWeight: "400" }}
+            >
+              {user.location.country}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "#979797", fontWeight: "400" }}
+            >
+              {user.email}
+            </Typography>
+          </Box>
+        ))
+      )}
     </section>
   );
 }
---
+++ app/hooks/useUsers.tsx
@@ -0,0 +1,24 @@
+import useSWR from "swr";
+import axios from "axios";
+
+// interface ApiProps {
+//   results: number;
+// }
+
+type results = number
+
+export default function useUsers(results: results) {
+  // TODO: Fix TypeScript error
+  // @ts-ignore
+  const fetcher = url => axios.get(url).then(response => response.data.results)
+  const { data, error, isLoading } = useSWR(
+    `https://randomuser.me/api/?results=${results}`,
+    fetcher
+  );
+
+  return {
+    users: data,
+    isLoading,
+    isError: error,
+  };
+}
