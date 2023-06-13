03616499be513ce44509b785a812811a0aba28dd
Luke Wong <lukewong93@gmail.com>
2023-06-13 18:18:18 +0800

    refactor: remove unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "./redux/provider";
 import { Inter } from "next/font/google";
 import NavBar from "./components/NavBar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={inter.className}>
         <NavBar />
         <Providers>{children}</Providers>
