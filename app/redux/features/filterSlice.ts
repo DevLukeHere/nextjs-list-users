2b7a7e3f8d9224427376903b47ed7751cfce037d
Luke Wong <lukewong93@gmail.com>
2023-06-26 22:18:06 +0800

    refactor: remove commented code

--- app/redux/features/filterSlice.ts
+++ app/redux/features/filterSlice.ts
@@ -1,62 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type FilterState = {
-  // users: {
-  //   cell: string;
-  //   dob: {
-  //     date: string;
-  //     age: number;
-  //   };
-  //   email: string;
-  //   gender: string;
-  //   id: {
-  //     name: string;
-  //     value: string;
-  //   };
-  //   location: {
-  //     city: string;
-  //     coordinates: {
-  //       latitude: string;
-  //       longitude: string;
-  //     };
-  //     country: string;
-  //     postcode: number;
-  //     state: string;
-  //     street: {
-  //       name: string;
-  //       number: number;
-  //     };
-  //     timezone: {
-  //       description: string;
-  //       offset: string;
-  //     };
-  //   };
-  //   login: {
-  //     md5: string;
-  //     password: string;
-  //     salt: string;
-  //     sha1: string;
-  //     sha256: string;
-  //     username: string;
-  //     uuid: string;
-  //   };
-  //   name: {
-  //     first: string;
-  //     last: string;
-  //     title: string;
-  //   };
-  //   nat: string;
-  //   phone: string;
-  //   picture: {
-  //     large: string;
-  //     medium: string;
-  //     thumbnail: string;
-  //   };
-  //   registered: {
-  //     age: number;
-  //     date: string;
-  //   };
-  // },
   name: string,
   users: any,
 };
